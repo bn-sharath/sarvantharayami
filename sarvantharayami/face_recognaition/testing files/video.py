d7b810c69cc73d80d4f1e4a158f28ebf475ad806
bn-sharath <sharathbn2001@gmail.com>
2022-07-14 22:39:35 +0530

    backend --> url routing

---
+++ sarvantharayami/face_recognaition/testing files/video.py
@@ -0,0 +1,12 @@
+import cv2
+
+capture_obj = cv2.VideoCapture("http://192.168.0.100:8080/video")
+
+while True:
+    r, frame = capture_obj.read()
+    cv2.imshow("hello", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture_obj.release()
+cv2.destroyAllWindows()
--- sarvantharayami/web_application/main.py
+++ sarvantharayami/web_application/main.py
@@ -1,14 +1,31 @@
-from flask import Flask
+from flask import Flask , render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "<h1>Hello, World!</h1>"
-
-@app.route("/home")
 def home():
-    return "<h1>Home page</h1>"
+    return render_template("home.html")
+
+@app.route("/service")
+def service():
+    return render_template("service.html")
+
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 
 
 
