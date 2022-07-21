9a9312208e9c0b55a77997cbe48cb92a72b88998
bn-sharath <sharathbn2001@gmail.com>
2022-07-21 21:38:32 +0530

    headache of register validation

--- sarvantharayami/web_application/main.py
+++ sarvantharayami/web_application/main.py
@@ -1,41 +1,63 @@
-from flask import Flask , render_template, url_for, request
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bns2001coder'
+
+
+# SqlAlchemy Database Configuration With Mysql
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://bns:bns2001coder@localhost/sdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), nullable=False)
+    secondName = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(15), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/service")
 def service():
     return render_template("service.html")
+
 
 @app.route("/blog")
 def blog():
     return render_template("blog.html")
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
-@app.route("/register", methods=['GET','POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     return render_template("register.html")
 
-@app.route("/otp" , methods=['GET', 'POST'])
+
+@app.route("/otp", methods=['GET', 'POST'])
 def otp_verify():
     return render_template("otp.html")
 
-@app.route("/forgot_pw" , methods=['GET', 'POST'])
+
+@app.route("/forgot_pw", methods=['GET', 'POST'])
 def forgot_pw():
     return render_template("forgot_pw.html")
-
-
-
 
 
 if __name__ == '__main__':
--- sarvantharayami/web_application/static/js/register.js
+++ sarvantharayami/web_application/static/js/register.js
@@ -7,18 +7,66 @@ let start = true;
 
 
 // showing the data
-let profile = document.getElementById('r-profile');
-let name = document.getElementById('r-name');
-let email = document.getElementById('r-email');
-let phone = document.getElementById('r-phone');
-let address = document.getElementById('r-address');
-let addhar = document.getElementById('r-addhar');
-let govID = document.getElementById('r-govID');
-let userid = document.getElementById('r-userid');
+let r_profile = document.getElementById('r-profile');
+let r_fname = document.getElementById('r-fname');
+let r_sname = document.getElementById('r-sname');
+let r_email = document.getElementById('r-email');
+let r_phone = document.getElementById('r-phone');
+let r_address = document.getElementById('r-address');
+let r_addhar = document.getElementById('r-addhar');
+// let govID = document.getElementById('r-govID');
+let r_userid = document.getElementById('r-userid');
 
+// input elments in forms
+let firstname = document.getElementById('firstname');
+let secondname = document.getElementById('secondname');
+let email = document.getElementById('email');
+let phone = document.getElementById('phone-no');
+let address = document.getElementById('address');
+let addhar_no = document.getElementById('addhar-no');
+let user_id = document.getElementById('user-id');
+let password = document.getElementById('password');
+let confirm_passowd = document.getElementById('confirm-password');
+let validation_error = document.getElementById('validation-error');
+
+let verify_flag = false;
+
+
+
+firstname.addEventListener("change", () => {
+
+    r_fname.querySelector("span").innerText = " = " + firstname.value;
+
+})
+secondname.addEventListener("change", () => {
+
+    r_sname.querySelector("span").innerText = " = " + secondname.value;
+
+})
+email.addEventListener("change", () => {
+    r_email.querySelector("span").innerText = " = " + email.value;
+})
+phone.addEventListener("change", () => {
+    r_phone.querySelector("span").innerText = " = " + phone.value;
+})
+address.addEventListener("change", () => {
+    r_address.querySelector("span").innerText = " = " + address.value;
+})
+addhar_no.addEventListener("change", () => {
+    r_addhar.querySelector("span").innerText = " = " + addhar_no.value;
+})
+user_id.addEventListener("change", () => {
+    r_userid.querySelector("span").innerText = " = " + user_id.value;
+})
+password.addEventListener("change", () => {
+    // r_fname.querySelector("span").innerText = password.value;
+})
+confirm_passowd.addEventListener("change", () => {
+    // r_fname.querySelector("span").innerText = confirm_passowd.value;
+})
 
 if (start) {
-    console.log("runing");
+    // console.log("runing");
     form_input.forEach((item) => {
         item.style.display = "none";
     });
@@ -26,6 +74,7 @@ if (start) {
     start = false;
     form_input[index].style.display = "block";
 }
+
 
 previous.addEventListener("click", () => {
     // console.log("working")
@@ -42,19 +91,35 @@ previous.addEventListener("click", () => {
             }
         }
         index = temp;
-        next.textContent="Next"
+        next.textContent = "Next"
     } else {
         previous.disabled = true;
     }
 });
 next.addEventListener("click", () => {
     // console.log("working")
-    
-    index = index + 1;
-    if (index < form_input.length  && index >= 0) {
+    let input_element = form_input[index].querySelectorAll('input')
+    // console.log(input_element[0].placeholder)
+    // console.log(input_element[0].id)
+    if(!verify_flag){
+        input_element.forEach(element => {
+            if((element.value == null) || (element.value.length <= 0) || (element.value == "")) {
+                // alert("Input is empty please Enter the valid input")
+                validation_error.innerHTML += "<li>Invalid!..Input is Empty " + element.placeholder +"<br></li>";
+                element.focus;
+                
+            }
+          
+            
+        });
+     
+    }
+
+
+    if (index < form_input.length && index >= 0) {
         // console.log(form_input[index])
         form_input[index].style.display = "block";
-        console.log(index);
+        // console.log(index);
         let temp = index;
         for (let index = 0; index < form_input.length; index++) {
             if (index != temp) {
@@ -62,23 +127,22 @@ next.addEventListener("click", () => {
             }
         }
         index = temp;
-        console.log(index);
+        // console.log(index);
         previous.disabled = false;
-        next.textContent="Next"
+        next.textContent = "Next"
     }
-    if (index==form_input.length-1) {
+    if (index == form_input.length - 1) {
 
         next.textContent = "Submit";
-        
+
     }
-    if (index >= form_input.length ) {
+    if (index >= form_input.length) {
         // console.log("working")
         // console.dir(next.type)
         next.type = "submit";
         // console.dir(next.type)
-    } 
+    }
+
 });
 
-function data_show() {
-    
-}
+
